6182a3e2e654044e706ce25e16388ca0859a8d1e
NatMiz <nathanael.mizutani@heig-vd.ch>
2020-03-09 16:31:15 +0100

    [WIP] station detection script

--- scripts/station-detection.py
+++ scripts/station-detection.py
@@ -4,3 +4,26 @@
 # Date: 09.03.2020
 # File: station-detection.py
 # Students: Stefan Dejanovic, Nathanael Mizutani
+
+# Sources: - IEEE Std 802.11-2016
+#          - https://gist.github.com/securitytube/5291959
+
+from scapy.all import *
+
+# Probe request subtype -> 0100
+if len(sys.argv) != 2:
+    print("usage : station-detection.py <interface>")
+    exit()
+
+interface = sys.argv[1]
+
+def PacketHandler(pkt) :
+
+  if pkt.haslayer(Dot11) :
+		if pkt.type == 0 and pkt.subtype == 4 :
+			if pkt.addr2 not in ap_list :
+				#ap_list.append(pkt.addr2)
+				print "AP MAC: %s with SSID: %s " %(pkt.addr2, pkt.info)
+
+
+sniff(iface=interface, prn = PacketHandler)
