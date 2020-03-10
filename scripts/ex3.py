bfecc0172bf8531509052ab39e1dbed759d12f2e
Edin Mujkanovic <edin.mujkanovic@protonmail.com>
2020-03-10 17:03:41 +0100

    Hidden ssid reveal

---
+++ scripts/ex3.py
@@ -0,0 +1,48 @@
+# Sources:
+# - https://docs.python.org/3/howto/curses.html
+# - https://www.devdungeon.com/content/curses-programming-python#toc-20
+
+
+import argparse
+
+from scapy.all import *
+from scapy.layers.dot11 import Dot11Beacon, Dot11ProbeResp, Dot11Elt, Dot11
+import curses
+from curses import *
+
+bssids = dict()
+
+screen = None
+
+def scanHiddenSSIDs(packet):
+    if(packet.haslayer(Dot11Elt)):
+        if packet.haslayer(Dot11ProbeResp) and packet[Dot11].addr3 in bssids:
+            bssids[packet[Dot11].addr3] = packet.info.decode()
+            printResults()
+        elif packet.haslayer(Dot11Beacon):
+            if(packet.info.decode() == b'\x00\x00\x00\x00\x00\x00\x00\x00'.decode('utf8')) and (packet[Dot11].addr3 not in bssids):
+
+                bssids[packet[Dot11].addr3] = "UNKNOWN SSID"
+                printResults()
+
+def printResults():
+    screen.clear()
+    screen.addstr(0,0, "Sniffing.... (CTRL + C to stop)")
+    screen.addstr(2,0, "BSSID\t\t\tSSID")
+    for ssid in bssids:
+        screen.addstr(3, 0, ssid + "\t" + bssids[ssid])
+    screen.refresh()
+
+def main(main_screen):
+    global screen
+    screen = curses.initscr()
+
+
+    parser = argparse.ArgumentParser(description='Script listant toutes les hiddens SSIDs')
+    parser.add_argument("--interface", required=True, help="Interface utilisée pour écouter")
+    args = parser.parse_args()
+
+
+    sniff(iface=args.interface, prn=scanHiddenSSIDs)
+
+wrapper(main)
