c3ef1b35e4b5989a795d1a813fcd42772c99d11a
NatMiz <nathanael.mizutani@heig-vd.ch>
2020-03-13 19:56:57 +0100

    Script can read probes

--- scripts/station-detection.py
+++ scripts/station-detection.py
@@ -17,13 +17,11 @@ if len(sys.argv) != 2:
 
 interface = sys.argv[1]
 
-def PacketHandler(pkt) :
-
-  if pkt.haslayer(Dot11) :
-		if pkt.type == 0 and pkt.subtype == 4 :
-			if pkt.addr2 not in ap_list :
+def PacketHandler(pkt):
+	if pkt.haslayer(Dot11):
+		if pkt.type==0 and pkt.subtype==4:
+			#if pkt.addr2 not in ap_list:
 				#ap_list.append(pkt.addr2)
-				print "AP MAC: %s with SSID: %s " %(pkt.addr2, pkt.info)
-
+			print("AP MAC: %s with SSID: %s " %(pkt.addr2, pkt.info))
 
 sniff(iface=interface, prn = PacketHandler)
