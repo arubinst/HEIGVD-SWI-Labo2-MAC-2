e4fc7a9b57e7467593ba17442445113d5b40cfc3
nyg <nyg@users.noreply.github.com>
2020-03-15 13:56:27 +0100

    Finished a) of 2.

---
+++ scripts/2-detect.py
@@ -0,0 +1,24 @@
+import argparse
+
+from scapy.all import *
+from scapy.layers.dot11 import Dot11ProbeReq, Dot11, Dot11Elt
+
+# Add the arguments to the parser
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--interface", required=True, help='Interface to use for sniffing')
+args = parser.parse_args()
+
+
+def packet_handler(packet):
+    dot11 = packet.getlayer(Dot11)
+    ssid = packet[Dot11Elt].info
+    print('STA {} sent probe req for AP {} (ssid: {})'.format(dot11.addr2, dot11.addr1, ssid))
+    # if ssid == b'':
+    #     print(packet.show())
+
+
+print('Started sniffing on interface', args.interface)
+sniff(iface=args.interface,
+      prn=packet_handler,
+      lfilter=lambda p: p.haslayer(Dot11ProbeReq) and p[Dot11Elt].ID == 0,
+      monitor=True)
