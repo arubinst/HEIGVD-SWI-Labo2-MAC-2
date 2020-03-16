ba74e3164b03860c84f20bdfc92b16ee8c69fc2a
Edin Mujkanovic <edin.mujkanovic@protonmail.com>
2020-03-16 16:44:14 +0100

    Commentary

--- scripts/ex2_1.py
+++ scripts/ex2_1.py
@@ -1,5 +1,4 @@
 from scapy.all import *
-import os
 import argparse
 from scapy.layers.dot11 import Dot11Beacon, Dot11, Dot11Elt, RadioTap, Dot11ProbeResp, Dot11ProbeReq
 
@@ -7,14 +6,23 @@ ssidTarget = ""
 stas = []
 
 def scanSTA(packet):
+    '''
+    Methode permettant de scanner les paquets à la recherche d'un SSID spécifique
+    :param packet: Paquet à analyser
+    '''
+
+    # On verifie si c'est une Probe request
     if Dot11ProbeReq in packet and Dot11Elt in packet[Dot11ProbeReq]:
-        #print(packet.show())
         originalPacket = packet
         packet = packet[Dot11ProbeReq]
         packet = packet[Dot11Elt]
+        # On regarde si le champs est celui qui contient le nom
         if (packet.ID == 0):
+            # On recupere le SSID du paquet
             ssidFound = packet.info.decode("utf-8")
+            # On recupere l'adresse Mac de l'emetteur de la probe request
             addr = originalPacket.addr2
+            # Si l'adresse MAC n'est pas déjà repertoriée et si le ssid trouvé et celui cherché, on ajoute l'adresse MAC à notre liste et on l'affiche
             if((addr not in stas) and (ssidFound == ssidTarget)):
                 stas.append(addr)
                 print(addr)
@@ -29,6 +37,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
     ssidTarget = args.ssid
 
+    #On lance le sniffing sans limite de temps
     print("Sniffing....")
     print("Ci-dessous, la liste des STAs cherchant à se connecter au SSID " + ssidTarget + " : ")
     sniff(iface=args.interface, prn=scanSTA)
--- scripts/ex2_2.py
+++ scripts/ex2_2.py
@@ -7,17 +7,22 @@ entries = dict()
 BANNED_MAC_ADDRESS = ["ff:ff:ff:ff:ff:ff", "00:00:00:00:00:00"]
 
 def scan(packet):
+    '''
+    Méthode permettant l'anaylse de paquet DATA afin de repertorier les AP ainsi que les STA communiquant avec ces dernières
+    :param packet: Paquet à analyser
+    '''
     addr1 = packet.addr1 #dest
     addr2 = packet.addr2 #source
     addr3 = packet.addr3 #bssid
 
+    # On vérifie que le packet est bien de type DATA
     if packet.type == 2:
         if addr1 is not None and addr2 is not None and addr3 is not None and addr1 not in BANNED_MAC_ADDRESS:
-
-            # On vérifie si le bssid est déjà présent
+            # On vérifie si le bssid est déjà présent, si non, on ajoute une netrée dans notre dictionnaire
             if addr3 not in entries:
                 entries[addr3] = set()
 
+            # Si l'adresse emettrice n'est pas égale au BSSID, on ajoute l'adresse dans l'entrée du dictionnaire correspondant au BSSID. Sinon on ajoute l'addr1.
             if addr2 != addr3 :
                 entries[addr3].add(addr2)
             else:
@@ -25,11 +30,16 @@ def scan(packet):
 
 
 def printResult():
+    '''
+    Méthode permettant d'afficher les résultats
+    '''
     print("Results : ")
     print("AP\t\t\t\tSTA")
+    # On parcourt les bssids
     for bssid in entries:
-        for ap in entries[bssid]:
-            print(bssid+"\t\t"+ap)
+        # On parcourt les STA correspondant au BSSID et on affiche
+        for sta in entries[bssid]:
+            print(bssid+"\t\t"+sta)
 
 
 
@@ -40,8 +50,10 @@ if __name__ == "__main__":
     parser.add_argument("--timeout", required=False, help="Timeout du sniff. Default is 15")
     args = parser.parse_args()
     timeout = 15
+    # Si l'argument timeout est spécifié par l'utilisateur on l'utilise, si non, par défaut c'est 15 secondes de sniff
     if(args.timeout is not None):
         timeout = args.timeout
+    # On démarre le sniffing
     print("Sniffing....")
     sniff(iface=args.interface,timeout=int(timeout), prn=scan)
     printResult()
