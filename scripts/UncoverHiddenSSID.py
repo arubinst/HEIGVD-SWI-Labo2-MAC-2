245a9a74e1c80da2d82c37fd7ba60cce89f2033e
manalito <aurelien.siu@heig-vd.ch>
2020-03-16 23:09:48 +0100

    finished step 3

--- scripts/UncoverHiddenSSID.py
+++ scripts/UncoverHiddenSSID.py
@@ -19,19 +19,21 @@ uncovered_ssids_aps = set()
 #           https://www.youtube.com/watch?v=_OpmfE43AiQ
 
 def packetHandler(pkt):
+    # Traitement si c'est un beacon frame
     if pkt.haslayer(Dot11Beacon):
         # Dresser une liste des SSID disponibles à proximité       
         if not pkt.info :
             if pkt.addr3 not in hidden_ssids_aps :
                 hidden_ssids_aps.add(pkt.addr3)
                 print("HIDDEN SSID Network Found! BSSID: ", pkt.addr3)
-    elif pkt.haslayer(Dot11ProbeResp) and ( pkt.addr3 in hidden_ssids_aps ) :
+    # Traitement s'il s'agit d'une probe request
+    elif Dot11ProbeResp in pkt and ( pkt.addr3 in hidden_ssids_aps ) :
         if pkt.addr3 not in uncovered_ssids_aps :
             uncovered_ssids_aps.add(pkt.addr3)
-            print("Uncovered HIDDEN SSID Network Found! ", pkt.info, pkt.addr3)
+            print("Uncovered HIDDEN SSID Network Found! ", pkt.info.decode(), pkt.addr3)
 
 
 print('Liste des AP invisibles')
 # On sniffe en passant en fonction de callback la fonction packetHandler
-sniff(count=300, iface=interface, prn=packetHandler)
+sniff(iface=interface, prn=packetHandler)
   
